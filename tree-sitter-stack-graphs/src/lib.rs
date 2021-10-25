ef6ff90646415906139935f419e42c114be774c2
Douglas Creager <dcreager@dcreager.net>
2021-10-25 16:15:44 -0400

    restructure

--- tree-sitter-stack-graphs/src/lib.rs
+++ tree-sitter-stack-graphs/src/lib.rs
@@ -5,18 +5,66 @@
 // Please see the LICENSE-APACHE or LICENSE-MIT files in this distribution for license details.
 // ------------------------------------------------------------------------------------------------
 
-use lsp_positions::Span;
 use lsp_positions::SpanCalculator;
 use stack_graphs::arena::Handle;
-use stack_graphs::arena::SupplementalArena;
 use stack_graphs::graph::File;
 use stack_graphs::graph::Node;
 use stack_graphs::graph::NodeID;
 use stack_graphs::graph::StackGraph;
 use thiserror::Error;
+use tree_sitter::Parser;
+use tree_sitter_graph::functions::Functions;
 use tree_sitter_graph::graph::Graph;
 use tree_sitter_graph::graph::GraphNode;
 use tree_sitter_graph::graph::GraphNodeRef;
+use tree_sitter_graph::Variables;
+
+pub struct StackGraphLanguage {
+    parser: Parser,
+    tsg: tree_sitter_graph::ast::File,
+    functions: Functions,
+    globals: Variables,
+}
+
+impl StackGraphLanguage {
+    pub fn from_language(language: tree_sitter::Language) -> StackGraphLanguage {
+        let mut parser = Parser::new();
+        parser.set_language(language).unwrap();
+        let tsg = tree_sitter_graph::ast::File::new(language);
+        // TODO: Load in TSG file from grammar repo
+        let functions = Functions::stdlib();
+        let globals = Variables::new();
+        StackGraphLanguage {
+            parser,
+            tsg,
+            functions,
+            globals,
+        }
+    }
+
+    pub fn load_stack_graph(
+        &mut self,
+        stack_graph: &mut StackGraph,
+        file: Handle<File>,
+        source: &str,
+    ) -> Result<(), LoadError> {
+        let tree = self
+            .parser
+            .parse(source, None)
+            .ok_or(LoadError::ParseError)?;
+        let graph = self
+            .tsg
+            .execute(&tree, source, &mut self.functions, &mut self.globals)?;
+        let mut loader = StackGraphLoader {
+            stack_graph,
+            file,
+            graph: &graph,
+            span_calculator: SpanCalculator::new(source),
+            nodes: Vec::new(),
+        };
+        loader.load()
+    }
+}
 
 /// An error that can occur while loading a stack graph from a TSG file
 #[derive(Debug, Error)]
@@ -29,43 +77,36 @@ pub enum LoadError {
     UnknownNodeType(String),
     #[error(transparent)]
     ExecutionError(#[from] tree_sitter_graph::ExecutionError),
+    #[error("Error parsing source")]
+    ParseError,
 }
 
-pub trait LoadGraph {
-    fn load_into_graph(
-        &self,
-        source: &str,
-        stack_graph: &mut StackGraph,
-        spans: &mut SupplementalArena<Node, Span>,
-        file: Handle<File>,
-    ) -> Result<(), LoadError>;
+struct StackGraphLoader<'a> {
+    stack_graph: &'a mut StackGraph,
+    file: Handle<File>,
+    graph: &'a Graph<'a>,
+    span_calculator: SpanCalculator<'a>,
+    nodes: Vec<Handle<Node>>,
 }
 
-impl LoadGraph for Graph<'_> {
-    fn load_into_graph(
-        &self,
-        source: &str,
-        stack_graph: &mut StackGraph,
-        spans: &mut SupplementalArena<Node, Span>,
-        file: Handle<File>,
-    ) -> Result<(), LoadError> {
-        let mut span_calculator = SpanCalculator::new(source);
-        let mut sg_nodes = Vec::with_capacity(self.node_count());
-        for (index, node_ref) in self.iter_nodes().enumerate() {
-            let node = &self[node_ref];
+impl<'a> StackGraphLoader<'a> {
+    fn load(&mut self) -> Result<(), LoadError> {
+        self.nodes.reserve(self.graph.node_count());
+        for (index, node_ref) in self.graph.iter_nodes().enumerate() {
+            let node = &self.graph[node_ref];
             let handle = match get_node_type(node, node_ref)? {
-                NodeType::Definition => load_definition(stack_graph, file, index, node, node_ref)?,
-                NodeType::DropScopes => load_drop_scopes(stack_graph, file, index),
-                NodeType::ExportedScope => load_exported_scope(stack_graph, file, index),
-                NodeType::InternalScope => load_internal_scope(stack_graph, file, index),
-                NodeType::JumpTo => load_jump_to_scope(stack_graph),
-                NodeType::PopSymbol => load_pop_symbol(stack_graph, file, index, node, node_ref)?,
-                NodeType::PushSymbol => load_push_symbol(stack_graph, file, index, node, node_ref)?,
-                NodeType::Reference => load_reference(stack_graph, file, index, node, node_ref)?,
-                NodeType::Root => load_root(stack_graph),
+                NodeType::Definition => self.load_definition(index, node, node_ref)?,
+                NodeType::DropScopes => self.load_drop_scopes(index),
+                NodeType::ExportedScope => self.load_exported_scope(index),
+                NodeType::InternalScope => self.load_internal_scope(index),
+                NodeType::JumpTo => self.load_jump_to_scope(),
+                NodeType::PopSymbol => self.load_pop_symbol(index, node, node_ref)?,
+                NodeType::PushSymbol => self.load_push_symbol(index, node, node_ref)?,
+                NodeType::Reference => self.load_reference(index, node, node_ref)?,
+                NodeType::Root => self.load_root(),
             };
-            load_span(spans, &mut span_calculator, self, node, handle)?;
-            sg_nodes.push(handle);
+            self.load_span(node, handle)?;
+            self.nodes.push(handle);
         }
         Ok(())
     }
@@ -111,130 +152,124 @@ fn get_node_type(node: &GraphNode, node_ref: GraphNodeRef) -> Result<NodeType, L
     }
 }
 
-fn load_definition(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-    node: &GraphNode,
-    node_ref: GraphNodeRef,
-) -> Result<Handle<Node>, LoadError> {
-    let symbol = match node.attributes.get("symbol") {
-        Some(symbol) => symbol.as_str()?,
-        None => return Err(LoadError::MissingSymbol(node_ref)),
-    };
-    let symbol = stack_graph.add_symbol(symbol);
-    let id = NodeID::new_in_file(file, index as u32);
-    Ok(stack_graph.add_pop_symbol_node(id, symbol, true).unwrap())
-}
+impl<'a> StackGraphLoader<'a> {
+    fn load_definition(
+        &mut self,
+        index: usize,
+        node: &GraphNode,
+        node_ref: GraphNodeRef,
+    ) -> Result<Handle<Node>, LoadError> {
+        let symbol = match node.attributes.get("symbol") {
+            Some(symbol) => symbol.as_str()?,
+            None => return Err(LoadError::MissingSymbol(node_ref)),
+        };
+        let symbol = self.stack_graph.add_symbol(symbol);
+        let id = NodeID::new_in_file(self.file, index as u32);
+        Ok(self
+            .stack_graph
+            .add_pop_symbol_node(id, symbol, true)
+            .unwrap())
+    }
 
-fn load_drop_scopes(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-) -> Handle<Node> {
-    let id = NodeID::new_in_file(file, index as u32);
-    stack_graph.add_drop_scopes_node(id).unwrap()
-}
+    fn load_drop_scopes(&mut self, index: usize) -> Handle<Node> {
+        let id = NodeID::new_in_file(self.file, index as u32);
+        self.stack_graph.add_drop_scopes_node(id).unwrap()
+    }
 
-fn load_exported_scope(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-) -> Handle<Node> {
-    let id = NodeID::new_in_file(file, index as u32);
-    stack_graph.add_exported_scope_node(id).unwrap()
-}
+    fn load_exported_scope(&mut self, index: usize) -> Handle<Node> {
+        let id = NodeID::new_in_file(self.file, index as u32);
+        self.stack_graph.add_exported_scope_node(id).unwrap()
+    }
 
-fn load_internal_scope(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-) -> Handle<Node> {
-    let id = NodeID::new_in_file(file, index as u32);
-    stack_graph.add_internal_scope_node(id).unwrap()
-}
+    fn load_internal_scope(&mut self, index: usize) -> Handle<Node> {
+        let id = NodeID::new_in_file(self.file, index as u32);
+        self.stack_graph.add_internal_scope_node(id).unwrap()
+    }
 
-fn load_jump_to_scope(stack_graph: &mut StackGraph) -> Handle<Node> {
-    stack_graph.jump_to_node()
-}
+    fn load_jump_to_scope(&mut self) -> Handle<Node> {
+        self.stack_graph.jump_to_node()
+    }
 
-fn load_pop_symbol(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-    node: &GraphNode,
-    node_ref: GraphNodeRef,
-) -> Result<Handle<Node>, LoadError> {
-    let symbol = match node.attributes.get("symbol") {
-        Some(symbol) => symbol.as_str()?,
-        None => return Err(LoadError::MissingSymbol(node_ref)),
-    };
-    let symbol = stack_graph.add_symbol(symbol);
-    let id = NodeID::new_in_file(file, index as u32);
-    if let Some(scoped) = node.attributes.get("scoped") {
-        if scoped.as_boolean()? {
-            return Ok(stack_graph
-                .add_pop_scoped_symbol_node(id, symbol, false)
+    fn load_pop_symbol(
+        &mut self,
+        index: usize,
+        node: &GraphNode,
+        node_ref: GraphNodeRef,
+    ) -> Result<Handle<Node>, LoadError> {
+        let symbol = match node.attributes.get("symbol") {
+            Some(symbol) => symbol.as_str()?,
+            None => return Err(LoadError::MissingSymbol(node_ref)),
+        };
+        let symbol = self.stack_graph.add_symbol(symbol);
+        let id = NodeID::new_in_file(self.file, index as u32);
+        if let Some(scoped) = node.attributes.get("scoped") {
+            if scoped.as_boolean()? {
+                return Ok(self
+                    .stack_graph
+                    .add_pop_scoped_symbol_node(id, symbol, false)
+                    .unwrap());
+            }
+        }
+        Ok(self
+            .stack_graph
+            .add_pop_symbol_node(id, symbol, false)
+            .unwrap())
+    }
+
+    fn load_push_symbol(
+        &mut self,
+        index: usize,
+        node: &GraphNode,
+        node_ref: GraphNodeRef,
+    ) -> Result<Handle<Node>, LoadError> {
+        let symbol = match node.attributes.get("symbol") {
+            Some(symbol) => symbol.as_str()?,
+            None => return Err(LoadError::MissingSymbol(node_ref)),
+        };
+        let symbol = self.stack_graph.add_symbol(symbol);
+        let id = NodeID::new_in_file(self.file, index as u32);
+        if let Some(scope) = node.attributes.get("scope") {
+            let scope = scope.as_graph_node()?;
+            let scope = NodeID::new_in_file(self.file, scope.index() as u32);
+            return Ok(self
+                .stack_graph
+                .add_push_scoped_symbol_node(id, symbol, scope, false)
                 .unwrap());
         }
+        Ok(self
+            .stack_graph
+            .add_push_symbol_node(id, symbol, false)
+            .unwrap())
     }
-    Ok(stack_graph.add_pop_symbol_node(id, symbol, false).unwrap())
-}
 
-fn load_push_symbol(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-    node: &GraphNode,
-    node_ref: GraphNodeRef,
-) -> Result<Handle<Node>, LoadError> {
-    let symbol = match node.attributes.get("symbol") {
-        Some(symbol) => symbol.as_str()?,
-        None => return Err(LoadError::MissingSymbol(node_ref)),
-    };
-    let symbol = stack_graph.add_symbol(symbol);
-    let id = NodeID::new_in_file(file, index as u32);
-    if let Some(scope) = node.attributes.get("scope") {
-        let scope = scope.as_graph_node()?;
-        let scope = NodeID::new_in_file(file, scope.index() as u32);
-        return Ok(stack_graph
-            .add_push_scoped_symbol_node(id, symbol, scope, false)
-            .unwrap());
+    fn load_reference(
+        &mut self,
+        index: usize,
+        node: &GraphNode,
+        node_ref: GraphNodeRef,
+    ) -> Result<Handle<Node>, LoadError> {
+        let symbol = match node.attributes.get("symbol") {
+            Some(symbol) => symbol.as_str()?,
+            None => return Err(LoadError::MissingSymbol(node_ref)),
+        };
+        let symbol = self.stack_graph.add_symbol(symbol);
+        let id = NodeID::new_in_file(self.file, index as u32);
+        Ok(self
+            .stack_graph
+            .add_push_symbol_node(id, symbol, true)
+            .unwrap())
     }
-    Ok(stack_graph.add_push_symbol_node(id, symbol, false).unwrap())
-}
 
-fn load_reference(
-    stack_graph: &mut StackGraph,
-    file: Handle<File>,
-    index: usize,
-    node: &GraphNode,
-    node_ref: GraphNodeRef,
-) -> Result<Handle<Node>, LoadError> {
-    let symbol = match node.attributes.get("symbol") {
-        Some(symbol) => symbol.as_str()?,
-        None => return Err(LoadError::MissingSymbol(node_ref)),
-    };
-    let symbol = stack_graph.add_symbol(symbol);
-    let id = NodeID::new_in_file(file, index as u32);
-    Ok(stack_graph.add_push_symbol_node(id, symbol, true).unwrap())
-}
+    fn load_root(&mut self) -> Handle<Node> {
+        self.stack_graph.root_node()
+    }
 
-fn load_root(stack_graph: &mut StackGraph) -> Handle<Node> {
-    stack_graph.root_node()
-}
-
-fn load_span(
-    spans: &mut SupplementalArena<Node, Span>,
-    span_calculator: &mut SpanCalculator,
-    graph: &Graph,
-    node: &GraphNode,
-    node_handle: Handle<Node>,
-) -> Result<(), LoadError> {
-    let source_node = match node.attributes.get("source_node") {
-        Some(source_node) => source_node.as_syntax_node(graph)?,
-        None => return Ok(()),
-    };
-    spans[node_handle] = span_calculator.for_node(source_node);
-    Ok(())
+    fn load_span(&mut self, node: &GraphNode, node_handle: Handle<Node>) -> Result<(), LoadError> {
+        let source_node = match node.attributes.get("source_node") {
+            Some(source_node) => source_node.as_syntax_node(self.graph)?,
+            None => return Ok(()),
+        };
+        *self.stack_graph.span_mut(node_handle) = self.span_calculator.for_node(source_node);
+        Ok(())
+    }
 }
